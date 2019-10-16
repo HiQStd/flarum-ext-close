29d3e86e71c26bd97cb17b44e050b086bb49191b
vetosers <vetosers@gmail.com>
2019-10-17 05:26:33 +0700

    Change version constraint in composer.json

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 # Changelog
 
-### [0.1.0](https://github.com/HiQStd/flarum-ext-close/tree/0.1.0)
-##### Changed
+### [0.1.1](https://github.com/HiQStd/flarum-ext-close/releases/tag/0.1.1)
+- Change version constraint
+
+### [0.1.0](https://github.com/HiQStd/flarum-ext-close/releases/tag/0.1.0)
 - Create extension.
--- extend.php
+++ extend.php
@@ -34,10 +34,10 @@ use Illuminate\Contracts\Events\Dispatcher;
 
 return [
     (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/resources/js/dist/forum.js')
+        ->js(__DIR__.'/js/dist/forum.js')
         ->css(__DIR__.'/resources/less/forum.less'),
     (new Extend\Frontend('admin'))
-        ->js(__DIR__.'/resources/js/dist/admin.js')
+        ->js(__DIR__.'/js/dist/admin.js')
         ->css(__DIR__.'/resources/less/admin.less'),
     new Extend\Locales(__DIR__ . '/resources/locale'),
     function (Dispatcher $events) {
